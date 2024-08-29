e15d50188899e3dd2eed791daf914614ba8a68b6
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2024-08-29 19:51:51 +0300

    Update SMC update script prgname.py to fetch version number from firmware

--- update/scripts/prgname.py
+++ update/scripts/prgname.py
@@ -1,16 +1,28 @@
-f = open("version.asm")
-
+# Init array
 version = {}
-version["version_major"] = 0
-version["version_minor"] = 0
-version["version_patch"] = 0
+version["version_major"] = -1
+version["version_minor"] = -1
+version["version_patch"] = -1
+
+# Parse version number from firmware source code
+try:
+    f = open("../version.h")
+except:
+    print("Could not open version.h")
+    exit()
 
 line = f.readline()
 while line:
     parts = line.split()
-    if len(parts) >= 3 and parts[1] == "=":
-        version[parts[0].strip().lower()] = int(parts[2].strip())
+    if len(parts) == 3:
+        if parts[0].lower() == "#define":
+            try:
+                version[parts[1].lower()] = int(parts[2])
+            except:
+                print("Error parsing: " + line)
     line = f.readline()
+
+f.close()
 
 # Return SMCUPDATE file name with version
 print("SMCUPDATE-" + str(version["version_major"]) + "." + str(version["version_minor"]) + "." + str(version["version_patch"]) + ".PRG")
